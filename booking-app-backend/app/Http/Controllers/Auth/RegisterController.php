67bdeaaa929fd5ca19fe20fbc47f3b9021f4f73e
wojteq89 <wojteq898@gmail.com>
2025-04-16 23:33:57 +0200

    Clear the code

--- booking-app-backend/app/Http/Controllers/Auth/RegisterController.php
+++ booking-app-backend/app/Http/Controllers/Auth/RegisterController.php
@@ -11,26 +11,24 @@ class RegisterController extends Controller
 {
     public function register(Request $request)
     {
-        // Walidacja, aby sprawdzić, czy pole 'phone_number' istnieje
         $validatedData = $request->validate([
             'first_name' => 'required|string',
             'second_name' => 'required|string',
             'email' => 'required|email|unique:users,email',
             'password' => 'required|string|min:6',
-            'role' => 'nullable|string|in:user,owner,admin',  // Zmieniamy na 'nullable'
+            'role' => 'nullable|string|in:user,owner,admin',
             'city' => 'required|string',
             'phone_number' => 'required|string',
         ]);
     
-        // Przypisanie domyślnej wartości dla 'role', jeśli nie została podana
-        $role = $validatedData['role'] ?? 'user';  // Jeśli brak 'role', ustawiamy domyślnie na 'user'
+        $role = $validatedData['role'] ?? 'user';
     
         $user = new User([
             'first_name' => $validatedData['first_name'],
             'second_name' => $validatedData['second_name'],
             'email' => $validatedData['email'],
             'password' => bcrypt($validatedData['password']),
-            'role' => $role,  // Używamy przypisanej wartości roli
+            'role' => $role,
             'city' => $validatedData['city'],
             'phone_number' => $validatedData['phone_number'],
         ]);
